e0b7833c76d11699d20d070978eb64c6a1d8f7fb
renjiehuang <930075924@qq.com>
2023-03-09 22:29:43 +0800

    HelloWorld

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -6,6 +6,15 @@ public class LeapYear {
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
+
+    private static boolean isLeapYear(int n) {
+        if (n % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
